083a4ee072b77e2d08777567a78d85833e5eb232
poropeza091293 <ompi152172@upemor.edu.mx>
2017-08-10 00:46:37 -0500

    inicio

---
+++ Assets/na.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class na : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	public void menu()
+	{
+		SceneManager.LoadScene ("uno");
+
+	}
+
+	public void camisas()
+	{
+		SceneManager.LoadScene ("camisas");
+
+	}
+
+	public void entrega()
+	{
+		SceneManager.LoadScene ("entrega");
+
+	}
+
+	public void salir()
+	{
+		Application.Quit ();
+
+	}
+
+	public void contacto()
+	{
+		Application.OpenURL( "tel://7772867988" );
+	}
+}
+
